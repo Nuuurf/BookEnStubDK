f80d56fd23315b3017ec52c2d0237743dbffc226
Jacob <33635489+jaggemand@users.noreply.github.com>
2023-11-10 11:26:11 +0100

    Added RUD for Booking

--- RestfulApi/DAL/DBBooking.cs
+++ RestfulApi/DAL/DBBooking.cs
@@ -1,6 +1,7 @@
 ﻿using RestfulApi.Models;
 using System.Data.SqlClient;
 using Dapper;
+using Microsoft.VisualBasic;
 
 namespace RestfulApi.DAL {
     public class DBBooking : IDBBooking {
@@ -12,20 +13,64 @@ namespace RestfulApi.DAL {
         }
 
         /// <summary>
+        /// Gets a single booking with the id matching the booking in the database.
+        /// </summary>
+        /// <param name="id"> the id of the booking object that needs to be retrieved</param>
+        /// <returns>A Booking object which is either null or not, depening if the object was found or not</returns>
+        public Booking GetSingleBooking(int id) {
+            string script = "SELECT * FROM Booking WHERE Id = @id";
+
+            Booking booking = null;
+
+            using (SqlConnection connection = _connectionString) {
+                booking = connection.QueryFirst<Booking>(script, new { id }); // Queries database for booking matching id and returns first result
+            }
+
+            return booking;
+        }
+
+        public bool UpdateBooking(Booking updatedBooking) {
+            string script = "UPDATE Booking SET TimeStart = @StartTime, TimeEnd = @EndTime, Notes = @Notes WHERE Id = @Id";
+            
+            int rowsAffected = 0;
+            
+            using (SqlConnection connection = _connectionString) {
+                rowsAffected = connection.Execute(script, updatedBooking);
+            }
+
+            return rowsAffected > 0;
+        }
+
+        /// <summary>
+        /// Deletes a booking with the id matching the booking in the database.
+        /// </summary>
+        /// <param name="id"> the id of the booking object that needs to be deleted</param>
+        /// <returns>A boolean indicading where the action was successful or not</returns>
+        public bool DeleteBooking(int id) {
+            string script = "DELETE FROM Booking WHERE Id = @id";
+
+            bool success = false;
+
+            using (SqlConnection connection = _connectionString) {
+                connection.Execute(script, new { id });
+                success = true;
+            }
+
+            return success;
+        }
+
+        /// <summary>
         /// Inserts a new booking with its values into the database.
         /// </summary>
         /// <param name="booking"> the booking object that needs to be persisted</param>
         /// <returns>A boolean indicading where the action was successful or not</returns>
         public bool CreateBooking(Booking booking) {
-            string script = "Insert into bookings TimeStart, TimeEnd, Notes values (@StartTime, @EndTime, @Notes)";
+            string script = "Insert into booking TimeStart, TimeEnd, Notes values (@StartTime, @EndTime, @Notes)";
 
             bool success = false;
 
-            using(SqlConnection connection = _connectionString) {
-                connection.Open();
-
+            using (SqlConnection connection = _connectionString) {
                 connection.Execute(script, booking);
-
                 success = true;
             }
 
