799e268fd345b09ce31f53b7013b06f2d85ec4c9
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-11-10 08:49:25 +0100

    Added connection test

--- TestProject/API/ConnectionTest.cs
+++ TestProject/API/ConnectionTest.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestfulApi.DAL;
 
 namespace TestProject.API {
-    internal class ConnectionTest {
+    public class ConnectionTest {
+
+        [Test]
+        public void CanConnectionBeEtablished_ShouldBeOpen() {
+            Assert.That(DBConnection.Instance.TryConnection()[0], Is.EqualTo(ConnectionState.Open));
+
+            TestContext.WriteLine(DBConnection.Instance.TryConnection()[0].ToString());
+        }
+
     }
 }
