1f6aede13b79bdcd7f083c1441fa57c46a219eef
Jacob <33635489+jaggemand@users.noreply.github.com>
2023-11-14 13:37:04 +0100

    View of Select Timeslot

--- RestfulApi/DAL/DBBooking.cs
+++ RestfulApi/DAL/DBBooking.cs
@@ -4,13 +4,11 @@ using Dapper;
 using Microsoft.VisualBasic;
 
 namespace RestfulApi.DAL {
-    public class DBBooking : IDBBooking
-    {
+    public class DBBooking : IDBBooking {
 
         private DBConnection conn;
 
-        public DBBooking()
-        {
+        public DBBooking() {
             conn = DBConnection.Instance;
         }
 
@@ -19,14 +17,12 @@ namespace RestfulApi.DAL {
         /// </summary>
         /// <param name="id"> the id of the booking object that needs to be retrieved</param>
         /// <returns>A Booking object which is either null or not, depening if the object was found or not</returns>
-        public Booking GetSingleBooking(int id)
-        {
+        public Booking GetSingleBooking(int id) {
             string script = "SELECT * FROM Booking WHERE Id = @id";
 
             Booking booking = null;
 
-            using (SqlConnection connection = conn.GetOpenConnection())
-            {
+            using (SqlConnection connection = conn.GetOpenConnection()) {
                 booking = connection.QueryFirst<Booking>(script,
                     new { id }); // Queries database for booking matching id and returns first result
             }
@@ -34,15 +30,13 @@ namespace RestfulApi.DAL {
             return booking;
         }
 
-        public bool UpdateBooking(Booking updatedBooking)
-        {
+        public bool UpdateBooking(Booking updatedBooking) {
             string script
                 = "UPDATE Booking SET TimeStart = @StartTime, TimeEnd = @EndTime, Notes = @Notes WHERE Id = @Id";
 
             int rowsAffected = 0;
 
-            using (SqlConnection connection = conn.GetOpenConnection())
-            {
+            using (SqlConnection connection = conn.GetOpenConnection()) {
                 rowsAffected = connection.Execute(script, updatedBooking);
             }
 
@@ -54,14 +48,12 @@ namespace RestfulApi.DAL {
         /// </summary>
         /// <param name="id"> the id of the booking object that needs to be deleted</param>
         /// <returns>A boolean indicading where the action was successful or not</returns>
-        public bool DeleteBooking(int id)
-        {
+        public bool DeleteBooking(int id) {
             string script = "DELETE FROM Booking WHERE Id = @id";
 
             bool success = false;
 
-            using (SqlConnection connection = conn.GetOpenConnection())
-            {
+            using (SqlConnection connection = conn.GetOpenConnection()) {
                 connection.Execute(script, new { id });
                 success = true;
             }
@@ -74,29 +66,25 @@ namespace RestfulApi.DAL {
         /// </summary>
         /// <param name="booking"> the booking object that needs to be persisted</param>
         /// <returns>A boolean indicading where the action was successful or not</returns>
-        public bool CreateBooking(Booking booking)
-        {
-            string script = "Insert into booking (TimeStart, TimeEnd, Notes, StubId) values (@TimeStart, @TimeEnd, @Notes, @StubId)";
+        public bool CreateBooking(Booking booking) {
+            string script = "Insert into Booking (TimeStart, TimeEnd, Notes, StubId) values (@TimeStart, @TimeEnd, @Notes, @StubId)";
 
             bool success = false;
 
-            using (SqlConnection con = conn.GetOpenConnection())
-            {
-                    con.Execute(script, booking);
-                    success = true;
-                }
-
-                return success;
+            using (SqlConnection con = conn.GetOpenConnection()) {
+                con.Execute(script, booking);
+                success = true;
             }
+
+            return success;
+        }
         // Not implemented
-        public List<Booking> GetBookingsInTimeslot(DateTime start, DateTime end)
-        {
+        public List<Booking> GetBookingsInTimeslot(DateTime start, DateTime end) {
             string script = "SELECT id, TimeStart, TimeEnd, Notes, StubId FROM Booking WHERE TimeStart < @TimeEnd AND TimeEnd > @TimeStart";
 
             List<Booking> bookings;
 
-            using (SqlConnection con = conn.GetOpenConnection())
-            {
+            using (SqlConnection con = conn.GetOpenConnection()) {
                 bookings = con.Query<Booking>(script, new { TimeStart = start, TimeEnd = end }).ToList();
 
             }
