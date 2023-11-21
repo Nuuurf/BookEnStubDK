ccf8751ac8b7270c7ebf77f5cc7e6219468cd126
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-11-21 12:29:27 +0100

    Added dynamic maxStubs method in test Db Utilities.cs

--- TestProject/API/DAO/Utilities.cs
+++ TestProject/API/DAO/Utilities.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,16 @@ namespace TestProject.API.DAO {
     public class Utilities {
 
         public int GetMaxDBStubs() {
-            // TODO :Implement this method to ask the datbaase for the count of stubs in the stub table
-            return 10; //Expected at most times
+
+            int maxStubs = 10; //what we assume is default if connection fails
+
+            using (IDbConnection con = DBConnection.Instance.GetOpenConnection()) {
+                string script = "Select count(*) from Stub";
+
+                maxStubs = con.Query<int>(script).FirstOrDefault();
+            } 
+                
+            return maxStubs; 
         }
     }
 }
