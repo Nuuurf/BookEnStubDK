3196cbe1d426ccfbb699106d482ebe5649f2dd50
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-11-27 10:51:40 +0100

    Added DTO static class, to convert from dtos to models in a more pretty way

---
+++ RestfulApi/DTOs/DTO.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.Hosting;
+using RestfulApi.Models;
+
+namespace RestfulApi.DTOs {
+    public static class DTO {
+
+        public static Customer ConvertToCustomer(DTOCustomer customerToConvert) {
+            return new Customer {
+                FirstName = customerToConvert.FullName,
+                Phone = customerToConvert.Phone,
+                Email = customerToConvert.Email
+            };
+        }
+
+        public static List<Booking> ConvertToBookingList(List<DTONewBooking> bookingToConvert) {
+            return bookingToConvert.Select(itemA => new Booking {
+                TimeStart = itemA.TimeStart,
+                TimeEnd = itemA.TimeEnd,
+                Notes = itemA.Notes,
+            }).ToList();
+        }
+    }
+}
