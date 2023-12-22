b736786860a91b2e0ef63ab6564707420f1eb9c1
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-12-22 15:11:39 +0100

    Updated ui and now uses local storage
    Uses AJAX call to fetch data every 10 seconds.
    Implemented localstorage for use in AJAX.
    Now using HtmlTag library to simplify and objectify html creation.

--- WebApplicationMVC/Controllers/OrderHistoryController.cs
+++ WebApplicationMVC/Controllers/OrderHistoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HtmlTags;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.ComponentModel;
 using System.Net;
@@ -59,13 +60,76 @@ namespace WebApplicationMVC.Controllers {
         }
 
         [HttpGet]
-        public IActionResult GetOrdersByPhone() {
-            Console.WriteLine("Called");
+        public async Task<IActionResult> GetOrdersByPhone(string phone) {
+            IActionResult result = null;
+
+            using(HttpClient client = new HttpClient()) {
+                string uri = "https://localhost:7021/Booking/" + phone; //test value
+
+                try {
+                    HttpResponseMessage response = await client.GetAsync(uri);
+
+                    if (response.IsSuccessStatusCode) {
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        List<BookingHistoryItem> items = JsonConvert.DeserializeObject<List<BookingHistoryItem>>(responseData);
+
+                        //sort items by date for better user experience
+                        items.Sort((x, y) => y.TimeStart.CompareTo(x.TimeStart));
+
+                        //Create a html table
+                        var table = new TableTag();
+                        table.AddClass("table table-boardered");
+
+                        //add row headers to match the prexisting table format
+                        table.AddHeaderRow(row => {
+                            row.Header("Booking id");
+                            row.Header("Noter");
+                            row.Header("Start tidspunkt");
+                            row.Header("Slut tidspunkt");
+                        });
+
+                        //insert the data from the booking history items
+                        foreach(BookingHistoryItem item in items) {
+                            //mark rows with bookings that are older than current date with red.
+                            if (item.TimeEnd < DateTime.Now) {
+                                table.AddBodyRow(row => {
+                                    row.Cell(item.Id + "").Attr("class", "text-danger text-lg");
+                                    row.Cell(item.Notes).Attr("class", "text-danger text-lg");
+                                    row.Cell(item.TimeStart.ToString()).Attr("class", "text-danger text-lg");
+                                    row.Cell(item.TimeEnd.ToString()).Attr("class", "text-danger text-lg");
+                                });
+                            }
+                            else {
+                                table.AddBodyRow(row => {
+                                    row.Cell(item.Id + "");
+                                    row.Cell(item.Notes);
+                                    row.Cell(item.TimeStart.ToString());
+                                    row.Cell(item.TimeEnd.ToString());
+                                });
+                            }
+                            
+                        }
+                        
+                        //convert table object into html content that can be sent to page.
+                        result = Content(table.ToHtmlString(), "text/html");
+                    }
+                    else {
+                        //return the status code if it is not successful
+                        return StatusCode((int) response.StatusCode);
+                    }
+                }
+                catch (Exception ex) {
+                    //return status code 500 if some exception occures.
+                    return StatusCode(500, "Some internal server error has occured " + ex.Message);
+                }
+            }
 
             //test string
-            string updatedHtml = "<tr><td>Booking 1</td><td>Details</td></tr><tr><td>Booking 2</td><td>Details</td></tr>";
+            //string updatedHtml = "<tr><td>Booking 1</td><td>Details</td></tr><tr><td>Booking 2</td><td>Details</td></tr>";
 
-            return Content(updatedHtml, "text/html"); // Return HTML content as a string
+            //return Content(updatedHtml, "text/html"); // Return HTML content as a string
+            return result;
         }
     }
 }
